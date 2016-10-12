56af728c3082cf2ac1ab6867d44ebfe26bd1fbe6
Ryan Bertram <rpbertram@users.noreply.github.com>
2016-10-11 20:27:14 -0500

    Completed basic program
    First challenge completed. Have not yet built out more complex version 2

--- rps.rb
+++ rps.rb
@@ -1,1 +1,25 @@
 # Rock, Paper, Scissors
+
+puts "Your move: rock, paper, or scissors?"
+user_move = gets.chomp
+puts "You chose #{user_move}."
+
+comp_move = ["rock", "paper", "scissors"].sample
+
+puts "Computer chose #{comp_move}."
+
+if user_move == comp_move
+  puts "It's a tie!"
+elsif user_move == "rock" && comp_move == "paper"
+  puts "You Lose!"
+elsif user_move == "rock" && comp_move == "scissors"
+  puts "You win!"
+elsif user_move == "paper" && comp_move == "rock"
+  puts "You win!"
+elsif user_move == "paper" && comp_move == "scissors"
+  puts "You lose!"
+elsif user_move == "scissors" && comp_move == "rock"
+  puts "You lose!"
+elsif user_move == "scissors" && comp_move == "paper"
+  puts "You win!"
+end
